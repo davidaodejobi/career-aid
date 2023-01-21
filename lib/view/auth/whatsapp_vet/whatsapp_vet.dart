2df44b8e8c6c5756f0c706cd576dc8ccf05fce2c
davidaodejobi <talk2engineerdavid@gmail.com>
2023-01-21 22:30:11 +0100

    feat: :zap: mobile verification checkpoint

--- lib/view/auth/whatsapp_vet/whatsapp_vet.dart
+++ lib/view/auth/whatsapp_vet/whatsapp_vet.dart
@@ -1,11 +1,13 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:career_aid/controller/legal/tac_contoller.dart';
+import 'package:career_aid/main.dart';
 import 'package:career_aid/view/legal/widget/tac_approval.dart';
 import 'package:flutter/material.dart';
 
 import 'package:career_aid/constant/constant.dart';
 import 'package:career_aid/view/shared/shared.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lottie/lottie.dart';
 
 class WhatsAppVet extends ConsumerWidget {
   const WhatsAppVet({super.key});
@@ -48,7 +50,11 @@ class WhatsAppVet extends ConsumerWidget {
               fgColor: !termProvider.termsAccepted
                   ? AppColor.kLightPrimaryColor.withOpacity(0.3)
                   : AppColor.kLightBackgroundColor,
-              onPressed: !termProvider.termsAccepted ? () {} : () {},
+              onPressed: !termProvider.termsAccepted
+                  ? () {}
+                  : () {
+                      successDialog(context);
+                    },
               title: 'Verify',
             ),
             const YMargin(16),
@@ -59,7 +65,11 @@ class WhatsAppVet extends ConsumerWidget {
               fgColor: !termProvider.termsAccepted
                   ? AppColor.kLightPrimaryColor.withOpacity(0.3)
                   : AppColor.kLightPrimaryColor,
-              onPressed: !termProvider.termsAccepted ? () {} : () {},
+              onPressed: !termProvider.termsAccepted
+                  ? () {}
+                  : () {
+                      skipWhatsAppVetting(context);
+                    },
               title: 'Skip',
             ),
             const TacApproval(),
@@ -68,4 +78,68 @@ class WhatsAppVet extends ConsumerWidget {
       ),
     );
   }
+}
+
+skipWhatsAppVetting(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text("Skip WhatsApp Vetting"),
+        content: const Text(
+            "Are you sure you want to skip the process of adding your number for sending updates? You will not receive updates on your application status"),
+        actions: <Widget>[
+          TextButton(
+            child: const Text("Yes"),
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: ((context) => const Home()),
+                ),
+              );
+            },
+          ),
+          ElevatedButton(
+            child: const Text("No"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+successDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(
+          "Successful",
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        content: Lottie.asset('assets/animations/success.json'),
+      );
+    },
+  );
+
+  // Wait for 3 seconds
+  Future.delayed(const Duration(seconds: 3)).then(
+    (_) {
+      // Close the dialog
+      Navigator.of(context).pop();
+
+      // Navigate to the new screen
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: ((context) => const Home()),
+        ),
+      );
+    },
+  );
 }
