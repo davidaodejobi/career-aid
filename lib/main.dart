2cd1effbcb1f928f922a047b10a0d4a03c7b3fa2
davidaodejobi <talk2engineerdavid@gmail.com>
2023-01-26 00:56:54 +0100

    chore: :heavy_plus_sign: set up firebase for the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:career_aid/firebase_options.dart';
 import 'package:career_aid/view/auth/signup/sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:career_aid/constant/keys.dart';
 import 'package:career_aid/controller/theme.dart';
@@ -15,6 +17,7 @@ HiveStorageService hiveStorageService = getIt<HiveStorageService>();
 void main() async {
   await ScreenUtil.ensureScreenSize();
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setup();
   await Hive.initFlutter();
   await Hive.openBox(box1);
